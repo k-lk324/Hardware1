6ec4fa45844d98f8d69a94f837c0dcb2f6877640
Katerina Papagiannitsi <aipapagi@ece.auth.gr>
2025-01-19 23:29:07 +0200

    Add funct3, funct7 assignments; placeholder ALUCtrl

--- exercise5/top_proc.v
+++ exercise5/top_proc.v
@@ -12,9 +12,12 @@ module top_proc #(parameter INITIAL_PC[31:0] = 32'h00400000)(
     output MemWrite,
     output reg [31:0] WriteBackData
 );
+
     wire Zero;
     wire [6:0] opcode;
-//change
+    wire [2:0] funct3; 
+    wire [6:0] funct7;
+
     reg PCSrc;
     reg ALUSrc;
     reg RegWrite;
@@ -22,7 +25,7 @@ module top_proc #(parameter INITIAL_PC[31:0] = 32'h00400000)(
     reg loadPC;
     reg [3:0] ALUCtrl;
 
-    datapath datapath(
+    datapath #(.INITIAL_PC(INITIAL_PC))datapath(
         .clk(clk),
         .rst(rst),
         .instr(instr),
@@ -48,10 +51,48 @@ module top_proc #(parameter INITIAL_PC[31:0] = 32'h00400000)(
     localparam [6:0] SW = 7'b0100011;
     localparam [6:0] BEQ = 7'b1100011;
     localparam [6:0] IMMEDIATE = 7'b0010011;
+    localparam [6:0] NON_IMMEDIATE = 7'b0110011;
+
+    // RISC-V funct3
+    // immediate
+    assign funct3 = instr[14:12];
+    localparam [2:0] FUNCT3_ADDI = 3'b000;
+    localparam [2:0] FUNCT3_SLTI = 3'b010;
+    localparam [2:0] FUNCT3_XORI = 3'b100;
+    localparam [2:0] FUNCT3_ORI = 3'b110;
+    localparam [2:0] FUNCT3_ANDI = 3'b111;
+    localparam [2:0] FUNCT3_SLLI = 3'b001;
+    localparam [2:0] FUNCT3_SRLI = 3'b101;
+    localparam [2:0] FUNCT3_SRAI = 3'b101;
+
+    // non-immediate
+    localparam [2:0] FUNCT3_ADD = 3'b000;
+    localparam [2:0] FUNCT3_SUB = 3'b000;
+    localparam [2:0] FUNCT3_SLL = 3'b001;
+    localparam [2:0] FUNCT3_SLT = 3'b010;
+    localparam [2:0] FUNCT3_XOR = 3'b100;
+    localparam [2:0] FUNCT3_SRL = 3'b101;
+    localparam [2:0] FUNCT3_SRA = 3'b101;
+    localparam [2:0] FUNCT3_OR = 3'b110;
+    localparam [2:0] FUNCT3_AND = 3'b111;
+
+    // RISC-V funct7
+    assign funct7 = instr[31:25];
+    localparam [6:0] FUNCT7_SRLI = 7'b0000000;
+    localparam [6:0] FUNCT7_SRAI = 7'b0100000;
+
+    localparam [6:0] FUNCT7_ADD = 7'b0000000;
+    localparam [6:0] FUNCT7_SUB = 7'b0100000;
+
+    localparam [6:0] FUNCT7_SRL = 7'b0000000;
+    localparam [6:0] FUNCT7_SRA = 7'b0100000;
 
 
 
-    // FSM
+
+
+
+    // FSM states
     reg [2:0] state;
     localparam [2:0] IF = 3'b000;
     localparam [2:0] ID = 3'b001;
@@ -59,6 +100,7 @@ module top_proc #(parameter INITIAL_PC[31:0] = 32'h00400000)(
     localparam [2:0] MEM = 3'b011;
     localparam [2:0] WB = 3'b100;
 
+    // FSM state logic
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             state <= IF;
@@ -66,19 +108,29 @@ module top_proc #(parameter INITIAL_PC[31:0] = 32'h00400000)(
             case (state)
                 IF: begin
                     // Instruction Fetch
+                    loadPC   <= 0;
+                    MemRead  <= 0;
+                    MemWrite <= 0;
+                    RegWrite <= 0;
+                    MemToReg <= 0;
+
                 end
-                ID: begin
-                    // Instruction Decode
-                end
-                EX: begin
-                    // Execute
-                end
+                ID: // Instruction Decode
+                EX: // Execute
                 MEM: begin
                     // Memory Access
+                    if (opcode == LW)
+                        MemRead  <= 1;
+                    else if (opcode == SW)
+                        MemWrite <= 1;
                 end
                 WB: begin
                     // Write Back
-
+                    loadPC <= 1;
+                    if (opcode == LW)
+                        MemToReg <= 1;
+                    if (opcode != SW && opcode != BEQ)
+                        RegWrite <= 1;
                     
                 end
             endcase
@@ -111,6 +163,21 @@ module top_proc #(parameter INITIAL_PC[31:0] = 32'h00400000)(
             end
             default: begin
                 ALUSrc = 0;
+            end
+        endcase
+    end
+
+    // ALUCtrl
+    always @(*) begin
+        case (opcode)
+            BEQ: begin
+                ALUCtrl = 4'b0110;
+            end
+            IMMEDIATE: begin
+                ALUCtrl = 4'b0010;
+            end
+            default: begin
+                ALUCtrl = 4'b0000;
             end
         endcase
     end
